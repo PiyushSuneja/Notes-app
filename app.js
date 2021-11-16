2cc6a05871adc805513069e936e4be4d8532bbef
PiyushSuneja <84224866+PiyushSuneja@users.noreply.github.com>
2021-11-16 20:33:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const yargs = require('yargs');
+const chalk = require('chalk');
+const notes = require('./notes.js');
+
+// 1. [] Add a note [id, body]
+// 2. [] Removing a note [id]
+// 3. [] Display all note []
+// 4. [] Reading a note [id] 
+
+yargs.command({
+    command: 'remove',
+    description: 'Remove the note with given id',
+    builder: {
+        id: {
+            type: 'string',
+            demandOption: true,
+            describe: 'Id of the note that needs to be removed',
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.id);
+    }
+});
+
+yargs.command({
+    command: 'add',
+    description: 'Adding a note',
+    builder: {
+        id : {
+            type: 'string',
+            demandOption: true,
+            describe: 'Add an id for your note'
+        },
+        content : {
+            type: 'string',
+            demandOption: true,
+            describe: 'Add some content for your note'
+        }
+    },
+    handler: function(argv) {
+        if (argv.content === '') {
+            console.log(chalk.red.inverse('Please supply valid content'));
+            return ;
+        }
+        let noteToAdd = {
+            id: argv.id,
+            content: argv.content,
+        };
+        notes.addNote(noteToAdd);
+    }
+});
+
+yargs.command({
+    command: 'list',
+    description: 'List all the notes',
+    handler: function() {
+        notes.showAllNotes();
+    }
+});
+
+yargs.command({
+    command: 'read',
+    description: 'Read note with given id',
+    builder: {
+        id: {
+            type: 'string',
+            demandOption: true,
+            describe: 'Read note with given id'
+        }
+    },
+    handler: function(argv) {
+        notes.readNote(argv.id);
+    }
+});
+
+yargs.version('notes version 2.1.0.windows.1');
+
+yargs.parse();  //node app.js --version and node app.js --help is command
---
+++ notes.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+// 1. [X] Add a note [id, body]
+// 2. [X] Removing a note [id]
+// 3. [X] Display all note []
+// 4. [X] Reading a note [id] 
+
+const filename = './notes.json'
+
+function readNote(id) {
+    let notes = loadNotes();
+    note = notes.find((note) => note.id == id);
+    if (note === undefined) {
+        console.log(chalk.inverse.red('Note not found'));
+        return ;
+    }
+    console.log(chalk.cyan.inverse(note.content));
+}
+
+function removeNote(id) {
+    let notes = loadNotes();
+    let filteredNotes = notes.filter(note => note.id != id);
+    if (notes.length == filteredNotes.length) {
+        console.log(chalk.red('Note not found!'));
+        return ;
+    }
+    saveNotes(filteredNotes);
+    console.log(chalk.green('Note was deleted!'));
+}
+
+function showAllNotes() {
+    let notes = loadNotes();
+    console.log(chalk.blue('Showing all notes..'));
+    notes.forEach(note => {
+        console.log(chalk.yellow(`${note.id}`))
+    });
+}
+
+// adds the note to the notes.json file
+function addNote(note) {
+    let allNotes = loadNotes();
+    let noteAlreadyExists = allNotes.some((exnote) => exnote.id == note.id);
+    if (noteAlreadyExists) {
+        console.log(chalk.red.inverse("Note with given id already exists!"));
+        return ;
+    }
+    allNotes = [...allNotes, note];
+    saveNotes(allNotes);
+    console.log(chalk.green.inverse('Note was added'));
+}
+
+// read form notes.json and return the notes as list
+function loadNotes() {
+    let dataBuffer = fs.readFileSync(filename);
+    let dataString = dataBuffer.toString();
+    return JSON.parse(dataString);
+}
+
+// save the list of notes to the notes.json file
+function saveNotes(notes) {
+    let notesJson = JSON.stringify(notes);
+    fs.writeFileSync(filename, notesJson);
+}
+
+module.exports = {
+    removeNote: removeNote,
+    showAllNotes: showAllNotes,
+    readNote: readNote,
+    addNote: addNote
+}
